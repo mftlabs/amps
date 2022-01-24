592a365feb6ac3ced527f9629245e5a1b83a31f5
aram0112 <48899851+aram0112@users.noreply.github.com>
2022-01-23 23:22:32 -0600

    Update run_script_action.ex

--- apps/amps/lib/actions/run_script_action.ex
+++ apps/amps/lib/actions/run_script_action.ex
@@ -17,6 +17,25 @@ defmodule RunScriptAction do
     case result do
       {:ok, rparm} ->
         Logger.info("Script returned: #{inspect(rparm)}")
+
+        if parms["send_output"] do
+          msgid = AmpsUtil.get_id()
+
+          event =
+            Map.merge(
+              msg,
+              %{
+                "msgid" => msgid,
+                "parent" => msg["msgid"],
+                "fpath" => msg["fpath"],
+                "ftime" => DateTime.to_iso8601(DateTime.utc_now()),
+                "fname" => AmpsUtil.format(parms["format"], msg)
+              }
+            )
+
+          AmpsEvents.send(event, parms, %{})
+        end
+
         :ok
 
       {:error, error} ->
