0d2a5d8cbe6ed07f766f5bffd38913fd086f8898
aram0112 <48899851+aram0112@users.noreply.github.com>
2022-02-15 12:20:44 -0600

    workflow update

--- apps/amps_web/test/amps_web/controllers/page_controller_test.exs
+++ apps/amps_web/test/amps_web/controllers/page_controller_test.exs
@@ -1,7 +1,7 @@
 defmodule AmpsWeb.PageControllerTest do
   use AmpsWeb.ConnCase
 
-  #Check api call /api/ampstest
+  # Check api call /api/ampstest
   test "GET /api/ampstest", %{conn: conn} do
     conn = get(conn, "/api/ampstest")
     assert json_response(conn, 200) == %{"message" => "Welcome to AMPS!"}
