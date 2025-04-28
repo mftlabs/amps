f2981063a383f1ca4fb49ed92eb16cde6212dd16
Stephen Noel <44347551+sdnoel@users.noreply.github.com>
2025-04-27 22:06:02 -0500

    updated elixir version 1.17
    updated elixir version to 1.17

--- apps/amps_web/mix.exs
+++ apps/amps_web/mix.exs
@@ -4,12 +4,12 @@ defmodule AmpsWeb.MixProject do
   def project do
     [
       app: :amps_web,
-      version: "0.1.0",
+      version: "0.1.1",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
       lockfile: "../../mix.lock",
-      elixir: "~> 1.12",
+      elixir: "~> 1.17",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
